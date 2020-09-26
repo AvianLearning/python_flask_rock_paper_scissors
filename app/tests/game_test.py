d2eb8b552c9c6bd05776a5d1c45dddb9e503862a
AvianLearning <iaradcliffe@hotmail.com>
2020-09-26 15:17:28 +0100

    Created and passed tests to check for draw and scissors beating paper

--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -7,7 +7,19 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.player_1 = Player("Player 1", "rock")
         self.player_2 = Player("Player 2", "scissors")
+        self.player_3 = Player("Player 3", "paper")
+        self.player_4 = Player("Player 4", "paper")
         self.game_1 = Game(self.player_1, self.player_2)
+        self.game_2 = Game(self.player_3, self.player_4)
+        self.game_3 = Game(self.player_2, self.player_3)
 
-    def test_can_find_winner(self):
+
+    def test_can_find_correct_winner_rock_scissors(self):
         self.assertEqual(self.player_1, self.game_1.find_winner(self.player_1, self.player_2))
+
+    def test_can_find_a_draw(self):
+        self.assertEqual(None, self.game_2.find_winner(self.player_3, self.player_4))
+
+    def test_can_find_correct_winner_scissors_paper(self):
+        self.assertEqual(self.player_2, self.game_3.find_winner(self.player_2, self.player_3))
+
